7bbf3aafd63ce8105b3d8916756d440b127ad717
Victor <victorfreiremartins@gmail.com>
2024-06-04 11:59:55 -0300

    Criando Carousel

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,21 @@ menuIcon.addEventListener('click', ()=> {
     navBg.classList.toggle('active');
 
 })
+
+// CAROUSEL
+let count = 1;
+
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    nextImage();
+}, 5000)
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+}
