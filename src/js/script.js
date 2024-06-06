9493f861a5dccd23755851be313aca534cf30575
Victor <victorfreiremartins@gmail.com>
2024-06-06 15:33:19 -0300

    Finalizando o Projeto

--- src/js/script.js
+++ src/js/script.js
@@ -27,3 +27,21 @@ function nextImage(){
 
     document.getElementById("radio" + count).checked = true;
 }
+
+// BUBBLES
+function createBubble(){
+    const body = document.querySelector('body')
+    const createElement = document.createElement('span')
+    var size = Math.random()*60;
+
+    createElement.style.width = 20 + size + 'px';
+    createElement.style.height = 20 + size + 'px';
+    createElement.style.left = Math.random() * innerWidth - 170 + "px";
+    body.appendChild(createElement);
+
+    setTimeout(()=>{
+        createElement.remove()
+    },4000)
+}
+
+setInterval(createBubble, 50)
